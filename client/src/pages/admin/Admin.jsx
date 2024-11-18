2264c0f01bcd28a64276543220dc7fe6572eeab4
sbrsh-2424 <sbrsh2424@gmail.com>
2024-11-18 09:56:52 +0530

    Added Admin

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
+import '../../styles/admin/admin.css' 
 
 const Admin = () => {
 
